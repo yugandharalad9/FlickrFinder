623a499252292e1f07123b470f1a4866ccd4d0f9
yugandharalad9 <30602646+yugandharalad9@users.noreply.github.com>
2017-12-21 15:58:13 -0500

    TextFieldDelegate on ViewController.swift file with keyboard notifications and tap gesture recognizer

--- FlickrFinder/ViewController.swift
+++ FlickrFinder/ViewController.swift
@@ -9,22 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //Mark: - Properties
+    
+    var keyboardOnScreen = false
+    
+    
     @IBOutlet weak var photoImageView: UIImageView!
-    
     @IBOutlet weak var photoTitleLabel: UILabel!
-    
     @IBOutlet weak var phraseTextField: UITextField!
-    
     @IBOutlet weak var phraseSearchButton: UIButton!
-    
     @IBOutlet weak var latitudeTextField: UITextField!
+    @IBOutlet weak var longitudeTextField: UITextField!
+    @IBOutlet weak var latLonSearchButton: UIButton!
+    @IBOutlet weak var currentLocationTextField: UITextField!
+    @IBOutlet weak var currentLocationSearchButton: UIButton!
     
+    //Mark: - Life Cycle
     override func viewDidLoad() {
-        
         super.viewDidLoad()
+        phraseTextField.delegate = self
+        latitudeTextField.delegate = self
+        longitudeTextField.delegate = self
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func userDidTapView(_ sender: Any) {
+        resignFirstResponder(phraseTextField)
+        resignFirstResponder(latitudeTextField)
+        resignFirstResponder(longitudeTextField)
+        resignFirstResponder(currentLocationTextField)
+    }
     
 
 
@@ -39,6 +55,44 @@ extension ViewController: UITextFieldDelegate {
         textField.resignFirstResponder()
         return true
         
+    }
+    
+    //Mark: Show/Hide Keyboard
+    
+    func keyboardWillShow(_ notification: Notification)  {
+        if !keyboardOnScreen {
+            view.frame.origin.y -= keyboardHeight(notification)
+        }
+    }
+    
+    func keyboardWillHide(_ notification: Notification)  {
+        if keyboardOnScreen {
+            view.frame.origin.y += keyboardHeight(notification)
+        }
+    }
+    
+    func keyboardDidShow(_ notification: Notification)  {
+        keyboardOnScreen = true
+    }
+    
+    func keyboardDidHide(_ notification: Notification)  {
+        keyboardOnScreen = false
+    }
+    
+    func resignFirstResponder(_ textField: UITextField)  {
+        if textField.isFirstResponder {
+            textField.resignFirstResponder()
+        }
+    }
+    
+    
+    
+    
+    
+    func keyboardHeight(_ notification: Notification) -> CGFloat {
+        let userInfo = (notification as NSNotification).userInfo
+        let keyboardSize = userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue
+        return keyboardSize.cgRectValue.height
     }
 }
 
