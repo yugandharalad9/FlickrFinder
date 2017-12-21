3dd2696888d283b98104e8a879c1bdc0a2db05e2
yugandharalad9 <30602646+yugandharalad9@users.noreply.github.com>
2017-12-21 13:36:24 -0500

    Outlets for Ui elements on Storyboard

--- FlickrFinder/ViewController.swift
+++ FlickrFinder/ViewController.swift
@@ -9,17 +9,49 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+    @IBOutlet weak var photoTitleLabel: UILabel!
+    
+    @IBOutlet weak var phraseTextField: UITextField!
+    
+    @IBOutlet weak var phraseSearchButton: UIButton!
+    
+    @IBOutlet weak var latitudeTextField: UITextField!
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+
+
+}
+
+//Mark: - ViewController: UITextFieldDelegate
+
+extension ViewController: UITextFieldDelegate {
+    
+    //Mark: UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+        
     }
+}
 
+//Mark: - ViewController (Notifications)
 
+private extension ViewController {
+    func subscribeToNotification(_ notification: Notification.Name, selector: Selector) {
+        NotificationCenter.default.addObserver(self, selector: selector, name: notification, object: nil)
+    }
+    
+    func unsubscribeFromAllNotifications()  {
+        NotificationCenter.default.removeObserver(self)
+    }
+    
 }
 
